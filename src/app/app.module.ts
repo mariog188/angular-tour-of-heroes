b9aec8b1bc45e1332398107c51220addb3de1650
Mario Gutierrez <mario.gutierrez@yuxiglobal.com>
2018-11-26 11:30:38 -0500

    The hero editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, HeroesComponent ],
+	imports: [ BrowserModule, FormsModule ],
+	providers: [],
+	bootstrap: [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {}
